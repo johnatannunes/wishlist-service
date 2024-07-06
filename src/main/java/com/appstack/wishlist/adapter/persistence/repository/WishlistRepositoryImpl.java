c1b2a664bd301d9de3663bf21eb8041172f8e50c
inup-systems <inupsystems@gmail.com>
2024-07-06 20:40:15 -0300

    [feat] - repositores

--- src/main/java/com/appstack/wishlist/adapter/persistence/repository/WishlistRepositoryImpl.java
+++ src/main/java/com/appstack/wishlist/adapter/persistence/repository/WishlistRepositoryImpl.java
@@ -2,9 +2,29 @@ package com.appstack.wishlist.adapter.persistence.repository;
 
 import com.appstack.wishlist.domain.model.Wishlist;
 import com.appstack.wishlist.domain.repository.WishlistRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
 public class WishlistRepositoryImpl implements WishlistRepository {
+
+    private final WishlistPersistenceRepository repository;
+
+    @Override
+    public Wishlist save(Wishlist wishlist) {
+        return repository.save(wishlist);
+    }
+
+    @Override
+    public Optional<Wishlist> findByUserId(String userId) {
+        return repository.findByUserId(userId);
+    }
 }
 
-interface SpringDataWishlistRepository extends MongoRepository<Wishlist, String> { }
+interface WishlistPersistenceRepository extends MongoRepository<Wishlist, String> {
+    Optional<Wishlist> findByUserId(String userId);
+}
--- src/main/java/com/appstack/wishlist/domain/repository/WishlistRepository.java
+++ src/main/java/com/appstack/wishlist/domain/repository/WishlistRepository.java
@@ -1,4 +1,10 @@
 package com.appstack.wishlist.domain.repository;
 
+import com.appstack.wishlist.domain.model.Wishlist;
+
+import java.util.Optional;
+
 public interface WishlistRepository {
+    Wishlist save(Wishlist wishlist);
+    Optional<Wishlist> findByUserId(String userId);
 }
