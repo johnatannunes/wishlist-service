bb4a6c7dfd55e7639497f717e2a59ebb13b958ca
inup-systems <inupsystems@gmail.com>
2024-07-07 06:52:25 -0300

    [feat] - improvement in the naming of mapper methods

--- src/main/java/com/appstack/wishlist/application/usecase/AddItemToWishlistUseCase.java
+++ src/main/java/com/appstack/wishlist/application/usecase/AddItemToWishlistUseCase.java
@@ -1,8 +1,11 @@
 package com.appstack.wishlist.application.usecase;
 
+import com.appstack.wishlist.adapter.mapper.ProductMapper;
 import com.appstack.wishlist.adapter.mapper.WishlistMapper;
+import com.appstack.wishlist.adapter.web.controller.dto.ProductRequest;
 import com.appstack.wishlist.adapter.web.controller.dto.WishlistResponse;
 import com.appstack.wishlist.domain.model.Product;
+import com.appstack.wishlist.domain.model.Wishlist;
 import com.appstack.wishlist.domain.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +15,12 @@ import org.springframework.stereotype.Service;
 public class AddItemToWishlistUseCase {
 
     private final WishlistService wishlistService;
+    private final ProductMapper productMapper;
     private final WishlistMapper wishlistMapper;
 
-    public WishlistResponse execute(String wishlistId, Product product) {
-       var wishlist = wishlistService.addItemToWishlist(wishlistId, product);
-       return wishlistMapper.toWishlistResponse(wishlist);
+    public WishlistResponse execute(String wishlistId, ProductRequest productRequest) {
+       Product product = productMapper.toDomain(productRequest);
+       Wishlist wishlist = wishlistService.addProductToWishlist(wishlistId, product);
+       return wishlistMapper.toResponse(wishlist);
     }
 }
