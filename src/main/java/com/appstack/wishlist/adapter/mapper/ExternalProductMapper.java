85483325f221516ab73e3834ef511fb0d6f9c179
inup-systems <inupsystems@gmail.com>
2024-07-08 02:31:12 -0300

    [feat] - separation of responsibility for obtaining the product

--- src/main/java/com/appstack/wishlist/adapter/mapper/ExternalProductMapper.java
+++ src/main/java/com/appstack/wishlist/adapter/mapper/ExternalProductMapper.java
@@ -2,7 +2,6 @@ package com.appstack.wishlist.adapter.mapper;
 
 import com.appstack.wishlist.adapter.external.dto.ExternalProductResponse;
 import com.appstack.wishlist.domain.model.ExternalProduct;
-import com.appstack.wishlist.domain.model.Product;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
